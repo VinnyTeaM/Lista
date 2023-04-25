4ef3271e3ff2a50bbd5b6c2a07d0b870f75692d6
avinn <avinn@Vinny-w11>
2023-04-25 12:07:08 -0300

    Commit initial

---
+++ Lista/src/app/Exemplo.java
@@ -0,0 +1,59 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("Maria");
+		list.add("Alex");
+		list.add("Bob");
+		list.add("Anna");
+		// recebe a posição e o elemento.
+		list.add(2, "Marco");
+
+		//tamanho da lista
+		System.out.println(list.size());
+		
+		
+		for(String x : list) {
+			System.out.println(x);
+		}
+		
+		System.out.println("-------------------");
+		
+		/*remove da lista
+		list.remove("Anna");
+		*/
+		
+		//remover com condição 
+		list.removeIf(x -> x.charAt(0) == 'M');
+		for(String x : list) {
+			System.out.println(x);
+		}
+		
+		System.out.println("-------------------");
+		// encontrar posição de um elemento, quando ele não encontra elemento retorna -1.
+		System.out.println("Index of Bob: " +list.indexOf("Bob"));
+	
+		System.out.println("-------------------");
+		/*adiciona com condição, pega lista converte para String,list stream faz operação lambda e Filter devolve String filtrado conforme a codição,
+		Collect converte stream para lista*/
+		List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+		for(String x : result) {
+		System.out.println(x);
+		}
+		
+		System.out.println("-------------------");
+		/*encontra elemento da lista com uma condição, chama findFirst vai pegar primeiro elemento da String que atenda a condição,
+		orElse se não existi retorna null.*/
+		String name = list.stream().filter(x -> x.charAt(0)== 'A').findFirst().orElse(null);
+		 System.out.println(name);
+	}
+
+}
---
+++ Lista/src/app/Program.java
@@ -0,0 +1,76 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		// PRIMEIRA PARTE: FAZENDO A LEITURA DOS DADOS.
+		System.out.print("How many employees will be registered? ");
+		int nId = sc.nextInt();
+		
+		for (int i=1; i<=nId; i++) {
+			System.out.println();
+			System.out.println("Employee #" + i + ": ");
+			
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			
+			while(hasId(list,id)) {
+					System.out.print("Id already taken. Try again: ");
+					id = sc.nextInt();
+			}
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
+		}
+		
+		// SEGUNDA PARTE: ATUALIZAÇÃO DE SALÁRIO DE UM DETERMINADO FUNCIONÁRIO.
+		
+		System.out.println();
+		System.out.println("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if (emp == null) {
+			System.out.println("This id does not exits!");
+		}
+		else {
+			System.out.println("Enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(percentage);
+		}
+		
+		// TERCEIRA PARTE: LISTANDO OS FUNCIONARIOS.
+		
+		System.out.println();
+		System.out.println("List of employees: ");
+		for (Employee obj : list) {
+			System.out.println(obj);
+		}
+		
+		sc.close();
+		
+		}
+		
+					
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
+	}
+
+}
